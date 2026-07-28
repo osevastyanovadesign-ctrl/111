81268492aeec654992789862a073b42fb8598a60
osevastyanovadesign-ctrl <o.sevastyanova.design@gmail.com>
2026-07-28 10:30:04 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,4 @@ import App from './App';
 
 import './index.css';
 
-
-const redirect = sessionStorage.getItem("redirect");
-
-if (redirect) {
-  sessionStorage.removeItem("redirect");
-  window.history.replaceState(null, "", "/111" + redirect);
-}
-
-
 createRoot(document.getElementById('root')!).render(<App />);
