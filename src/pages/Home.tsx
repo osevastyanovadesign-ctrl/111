73d33f9a5f67458ac0e3aefcea07512e22dd12a9
osevastyanovadesign-ctrl <o.sevastyanova.design@gmail.com>
2026-07-28 16:09:01 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -64,7 +64,7 @@ export default function Home() {
 
   useEffect(() => {
     if ("scrollRestoration" in history) {
-      history.scrollRestoration = "auto";
+      history.scrollRestoration = "manual";
     }
   }, []);
 
